aaaa1d14b4321d260dfdd828fd97b05f92117853
Parth Shah <parthshah2065@outlook.com>
2022-04-13 15:42:08 +0100

    Update FileUpload.java

--- grpc-client/src/main/java/com/example/grpc/client/grpcclient/FileUpload.java
+++ grpc-client/src/main/java/com/example/grpc/client/grpcclient/FileUpload.java
@@ -13,8 +13,8 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.multipart.MultipartFile;
 
 @Controller
-public class FileUploadController {
-  public static String uploadDirectory = System.getProperty("user.dir")+"/uploads";
+public class FileUploadController{
+  public static String uploadDirectory = System.getProperty("user.dir")+"/uploadedmatrixfiles";
 /*  @RequestMapping("/")
   public String UploadPage(Model model) {
 	  return "uploadview";
